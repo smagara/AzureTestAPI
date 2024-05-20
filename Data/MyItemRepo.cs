067126278212c7fd4b6bd29c96ae3c777449796c
smagara <smagara@hotmail.com>
2024-05-19 23:53:21 -0400

    Checkpoint 2 works earth and cloud

--- Data/MyItemRepo.cs
+++ Data/MyItemRepo.cs
@@ -15,10 +15,20 @@ namespace azure_test_api.Data
     {
 
         private readonly IConfiguration configuration;
+        private readonly string azureClientId = "";
+        private readonly string azureSQLAuthURL = "";
 
         public MyItemRepo(IConfiguration config)
         {
             configuration = config;
+
+            azureClientId = configuration.GetValue<string?>("AzureSettings:ClientID") ?? "";
+            azureSQLAuthURL = configuration.GetValue<string?>("AzureSettings:AzureSQLAuthURL") ?? "";
+
+            if (azureClientId == "" || azureSQLAuthURL == "")
+            {
+                throw new ArgumentNullException("AzureSettings:ClientID or AzureSettings:AzureSQLAuthURL is not set in appsettings.json");
+            }
         }
 
         public async Task<IEnumerable<MyItem>> GetItems()
@@ -30,20 +40,26 @@ namespace azure_test_api.Data
             {
                 // connect from desktop
                 connectionString = configuration.GetConnectionString("DevConnection");
+
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    return await connection.QueryAsync<MyItem>(sql);
+                }
             }
             else
             {
                 // connection from within Azure
                 connectionString = configuration.GetConnectionString("DefaultConnection");
+
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    var credential = new ManagedIdentityCredential(azureClientId);
+                    var token = await credential.GetTokenAsync(new Azure.Core.TokenRequestContext(new[] { "https://database.windows.net/.default" }));
+                    connection.AccessToken = token.Token;
+                    return await connection.QueryAsync<MyItem>(sql);
+                }
             }
 
-            using (var connection = new SqlConnection(connectionString))
-            {
-                var credential = new ManagedIdentityCredential("5cae1e9c-aa36-4c89-963f-5930f751e41a");
-                var token = await credential.GetTokenAsync(new Azure.Core.TokenRequestContext(new[] { "https://database.windows.net/.default" }));
-                connection.AccessToken = token.Token;
-                return await connection.QueryAsync<MyItem>(sql);
-            }
         }
 
     }
